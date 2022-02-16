b5c23fafa77a3d921c222893c920578866f6bb69
Ankit Solanki <ankit_ms95@yahoo.com>
2022-02-16 22:17:48 +0530

    Initial commit

---
+++ app/src/main/java/com/adfoodz/currentlocation/MainActivity.java
@@ -0,0 +1,110 @@
+package com.adfoodz.currentlocation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+
+import static androidx.constraintlayout.motion.widget.Debug.getLocation;
+
+public class MainActivity extends AppCompatActivity {
+
+    FusedLocationProviderClient fusedLocationClient;
+    LocationCallback mlocationCallback;
+    LocationRequest locationRequest;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+
+        mlocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    Log.e("locationResult", "null");
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    Log.e("CONTINIOUSLOC: ", location.toString());
+                    textView.setText(location.getLatitude() + "," + location.getLongitude());
+                }
+            }
+        };
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            getLocation();
+        } else {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 3);
+        }
+    }
+
+    public void getLocation() {
+        locationRequest = LocationRequest.create()
+                .setInterval(10)
+                .setFastestInterval(10)
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+
+        LocationServices
+                .getSettingsClient(MainActivity.this)
+                .checkLocationSettings(builder.build())
+                .addOnSuccessListener(MainActivity.this, (LocationSettingsResponse response) -> {
+                    if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                            ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                    fusedLocationClient.requestLocationUpdates(locationRequest, mlocationCallback, Looper.getMainLooper());
+                })
+                .addOnFailureListener(MainActivity.this, ex -> {
+                    if (ex instanceof ResolvableApiException) {
+                        try {
+                            ResolvableApiException resolvable = (ResolvableApiException) ex;
+//                            resolvable.startResolutionForResult(getActivity(), 1);  // for activity
+                            startIntentSenderForResult(resolvable.getResolution().getIntentSender(), 1, null, 0, 0, 0, null); // for fragment
+                        } catch (IntentSender.SendIntentException sendEx) {
+                        }
+                    }
+                });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 3) {
+            if (permissions[0].equals(Manifest.permission.ACCESS_FINE_LOCATION)
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    permissions[1].equals(Manifest.permission.ACCESS_COARSE_LOCATION)
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                getLocation();
+            }
+        }
+    }
+}
